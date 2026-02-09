f396e2d4998cf06e9eada416b43d8e7fc64720c4
Ramit Aggarwal <scaldcrab@gmail.com>
2026-02-09 15:47:20 +0530

    minor updations

---
+++ chess_app/.next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"MXqJ/qXKOfWcHN4ASVr2Tbe4INU3DNoOqc/Dmph+iKQ=\"\n}"
--- chess_app/app/components/chessboard.tsx
+++ chess_app/app/components/chessboard.tsx
@@ -5,12 +5,25 @@ export default function ChessBoard({board}:{
             square: Square;
             type: PieceSymbol;
             color: Color;
-        } | null)[][];
+        } | null)[][]; // 2d- array
     }){
     return(
         <>
             <div className="w-120 h-120 bg-blue-400 grid grid-cols-8 grid-rows-8 border ">
-                
+                {
+                    board.map((row,i)=>{
+                        return <div key={i} className="flex">
+                            {
+                                row.map((square,j)=>(
+                                    <div key={j} className={`w-8 h-8 ${(i+j)%2===0 ? 'bg-green-500': 'bg-green-100' }`} > 
+                                         {square ? square.type : ""}
+                                    </div>
+                                ))
+                            } 
+
+                        </div>
+                    })
+                }
             </div>   
         </>
     )
